bc8091b2854d015adbd7ff1782f72a84ece4638a
Fernando Vargas <fvargaspf@gmail.com>
2020-09-21 17:19:08 -0300

    feat: migration para usuarios

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,11 @@
 const Sequelize = require('sequelize'),
 	  dbConfig = require('../config/database'),
 	  connection = new Sequelize(dbConfig),
-	  Produto = require('../models/Produto')
+	  Produto = require('../models/Produto'),
+	  Usuario = require('../models/Usuario')
 ;
 
 Produto.init(connection);
+Usuario.init(connection);
 
 module.exports = connection;
---
+++ src/database/migrations/20200921183852-create-usuarios.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = {
+  	up: (queryInterface, Sequelize) => {
+    	return queryInterface.createTable('usuarios', {
+			id: {
+				type: Sequelize.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+				allowNull: false,
+			},
+			nome: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			email: {
+				type: Sequelize.STRING,
+				allowNull: false,
+				unique: true
+			},
+			telefone: {
+				type: Sequelize.STRING,
+				allowNull: false,
+				unique: true
+			},
+			senha: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			endereco: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			created_at: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+			updated_at: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+		});
+	},
+
+	down: (queryInterface, Sequelize) => {
+    	return queryInterface.dropTable('usuarios');
+  	}
+};
