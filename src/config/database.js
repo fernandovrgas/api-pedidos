8b782f96632e0947af92ae5ed096c0d9b25ea334
Fernando Vargas <fvargaspf@gmail.com>
2020-09-21 14:03:09 -0300

    feat: ajuste de nome de banco

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
 	host: 'localhost',
 	username: 'root',
 	password: 'A3dudfemf',
-	database: 'node-api-mysql',
+	database: 'api-pedidos',
 
 	define: {
 		timestamps: true, // cria created_at e updated_at automaticamente
